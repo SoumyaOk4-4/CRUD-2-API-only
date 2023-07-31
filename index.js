c1678596ca6cb5356b1e78563b26483ff041a033
Soumya Chakraborty <94857677+SoumyaOk4-4@users.noreply.github.com>
2023-07-31 19:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
